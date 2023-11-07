3021fe151a43308bbbbc4a35d4c9c3238b455286
ScrobeZ <jorgeluis-sc@outlook.es>
2023-11-06 19:46:58 -0800

    Se borro un archivo inecesario

--- lib/presentation/providers/chats/chat_provider.dart
+++ lib/presentation/providers/chats/chat_provider.dart
@@ -6,23 +6,7 @@ import '../../../domain/dtos/message.dart';
 class ChatProvider extends ChangeNotifier {
   final chatScrollController = ScrollController();
   final getYesNoAnswer = GetYesNoAnswer();
-  List<Message> messageList = [
-    // Message(text: 'HolaAmor!', fromWho: FromWho.me),
-    // Message(text: 'Ya regresaste ?', fromWho: FromWho.me),
-    // Message(
-    //     text: 'no bb!',
-    //     fromWho: FromWho.hers,
-    //     imageUrl:
-    //         'https://yesno.wtf/assets/no/13-755222c98795431aa2e7d453ab1e75a1.gif'),
-    // Message(text: 'Ya regresaste ?', fromWho: FromWho.me),
-    // Message(text: 'Pasare programación Mobil ?', fromWho: FromWho.me),
-    // Message(
-    //     text: 'nel paste!',
-    //     fromWho: FromWho.hers,
-    //     imageUrl:
-    //         'https://yesno.wtf/assets/no/13-755222c98795431aa2e7d453ab1e75a1.gif'),
-    // Message(text: 'que malvado', fromWho: FromWho.me),
-  ];
+  List<Message> messageList = [];
 
   Future<void> herResponse() async {
     final herMessage = await getYesNoAnswer.getAnswer();
